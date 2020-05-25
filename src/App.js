487fb78cac5cdb4fff9fdb9aee7b6c9745034d83
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-05-26 01:54:51 +0530

    Added various components needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from '../src/Components/Navbar/navbar';
+import Footer from '../src/Components/Footer/footer';
+import Landing from '../src/Components/Landing/landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NavBar />
+    <Landing />
+    <Footer />
   );
 }
 
