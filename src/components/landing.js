6fda3af99f215bfd94b5f96af07642b8ff5cd82b
Shashwat Awasthi <53440849+ShashwatAwasthi04@users.noreply.github.com>
2020-06-08 17:02:24 +0530

    Added landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -6,14 +6,42 @@ class landing extends Component{
         return(
             <div style={{width: '100%', margin: 'auto'}}>
                 <Grid className="grid1">
-                    <Cell col={12}>
+                    <Cell col={5}>
                         <img
                             src={Shashwat}
-                            alt="Picture of him"
+                            alt="Picture of me"
                             className="myPic"
                         />
+                        </Cell>
+                        <Cell col={6}>
                         <div className="myText">
-                            <h1>MERN stack developer</h1>
+                            <h4>Shashwat Awasthi</h4>
+                            <p>A second Year Undergraduate student</p>
+                            <hr />
+                            <h4>Stream:-</h4>
+                            <p>Computer Science and Engineering</p>
+                            <hr />
+                            <h4>National Institute Of Technology,Raipur</h4>
+                        </div>
+                        </Cell>
+                </Grid>
+                <Grid>
+                <Cell col={12}>
+                        <div className="myText">
+                            <h4>MERN stack developer</h4>
+                            
+                            
+                            <p>React JS || JavaScript || Bootstrap || HTML,CSS || Node JS|| Express JS</p>
+                            
+                            <hr />
+
+                            <h4>Competitive Programmer</h4>
+                            <p>JAVA</p>
+
+                            
+                            <hr />
+                            <h4>Data Analytics Enthusiast</h4>
+                            <p>Data Analysis using SQl</p>
                             
                         </div>
                     </Cell>
